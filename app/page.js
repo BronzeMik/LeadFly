b7843d383b55388b7e9eba6fe675c3e0f7e7fb57
Mikaila Brown <mikaila.brown2022@gmail.com>
2024-07-20 22:05:59 -0400

    Update Image alt

--- app/page.js
+++ app/page.js
@@ -31,7 +31,7 @@ export default function Home() {
             <a href="http://app.leadflyapp.com" target='_blank' className="bg-accent border-2 text-white px-4 py-2 rounded hover:bg-white hover:text-accent hover:border-accent">START FREE TRIAL</a>
           </div>
           <div>
-            <Image src='/hero-img.png' />
+            <Image src='/hero-img.png' alt="LeadFly dashboard"/>
           </div>
         </div>
           
